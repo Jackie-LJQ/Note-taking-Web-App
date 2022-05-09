b0c166df5dbce30841a41651c3d87d44e8f8d136
Hanyang Zhao <hzhao077@usc.edu>
2022-05-08 20:01:59 -0700

    To-do list item max length and hover

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import "./TodoItem.css"
+import React from "react";
+import "./TodoItem.css";
 
-export default function TodoItem({items, completeItem, deleteItem}) {
-  return (
-      items.map((item)=>(
-        <div key={item._id}>
-        <div className={item.isCompleted ? "completedcontent" : "incompletedcontent"}>{item.content}</div>      
-        <i className="icon check fa-solid fa-circle-check" 
-            onClick={()=>completeItem(item._id, item.isCompleted)}>
-        </i>
-        <i className="icon trash fa-solid fa-trash-can"
-            onClick={()=>deleteItem(item._id)}>
-        </i>
+export default function TodoItem({ items, completeItem, deleteItem }) {
+  return items.map((item) => (
+    <div key={item._id}>
+      <div className="singleRow">
+        <div className="itemWrapper">
+          <div
+            className={
+              item.isCompleted ? "completedcontent" : "incompletedcontent"
+            }
+            title={item.content}
+          >
+            {item.content}
+          </div>
         </div>
-  ))      
-  )
+        <div className="iconWrapper">
+          <i
+            className="icon check fa-solid fa-circle-check"
+            onClick={() => completeItem(item._id, item.isCompleted)}
+          ></i>
+          <i
+            className="icon trash fa-solid fa-trash-can"
+            onClick={() => deleteItem(item._id)}
+          ></i>
+        </div>
+      </div>
+    </div>
+  ));
 }
